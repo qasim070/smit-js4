995ccf98a5697db804d7a08945e66ce066fbc52e
qasim070 <62395180+qasim070@users.noreply.github.com>
2023-08-10 01:08:24 +0500

    created functions and buttons

---
+++ app.js
@@ -0,0 +1,44 @@
+let task1 = () => {
+    let num = [1,2,3,4,6,8,9];
+    let result = num.filter( function(e){
+        return e % 2 === 0;
+    });
+    alert( "Even Numbers are " + result + " In this Array");
+
+}
+let task2 = () => {
+    alert("Task task2 ")
+    
+}
+let task3 = () => {
+    alert("Task task3 ")
+    
+}
+let task4 = () => {
+    alert("Task task4 ")
+    
+}
+let task5 = () => {
+    alert("Task task5 ")
+    
+}
+let task6 = () => {
+    alert("Task task6 ")
+    
+}
+let task7 = () => {
+    alert("Task task7 ")
+    
+}
+let task8 = () => {
+    alert("Task task8 ")
+    
+}
+let task9 = () => {
+    alert("Task task9 ")
+    
+}
+let task10 = () => {
+    alert("Task task10")
+    
+}
